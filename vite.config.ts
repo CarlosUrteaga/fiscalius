924e1ca0758811e5b99ed6aa877fb42f478c3794
Carlos Urteaga-Reyesvera <carlos.urteaga@outlook.com>
2026-03-07 13:27:21 -0600

    mod vit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fiscalius/', // Ruta fija para urteaga.tech/fiscalius/
+  base: '/fiscalius/', // Esto debe ser EXACTO al nombre del repo en GitHub
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })

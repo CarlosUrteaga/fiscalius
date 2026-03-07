9c12289bbd94bc8a4fa053ac10c5e0f06d88ea48
Carlos Urteaga-Reyesvera <carlos.urteaga@outlook.com>
2026-03-07 13:33:44 -0600

    modify index and vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fiscalius/', // Esto debe ser EXACTO al nombre del repo en GitHub
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  }
+  base: './', // Esto asegura que todas las rutas sean relativas al directorio actual
 })
